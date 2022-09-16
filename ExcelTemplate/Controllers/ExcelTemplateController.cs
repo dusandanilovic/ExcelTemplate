12050b88ac974206a5847926dd637f5cd454ca08
dusan-danilovic-cif <dusan.danilovic@cif.co.rs>
2022-09-16 12:13:05 +0200

    dll

--- ExcelTemplate/Controllers/ExcelTemplateController.cs
+++ ExcelTemplate/Controllers/ExcelTemplateController.cs
@@ -1,6 +1,8 @@
 using ExcelTemplate.Data;
 using Microsoft.AspNetCore.Mvc;
 using System.Configuration;
+using StagingAlgorithm_newdata;
+using MathWorks.MATLAB.NET.Arrays;
 
 namespace ExcelTemplate.Controllers
 {
@@ -15,13 +17,27 @@ namespace ExcelTemplate.Controllers
             Configuration = configuration;
         }
 
-        [HttpGet("GetValidation")]
+        [HttpGet("GetTemplate")]
         public async Task<ActionResult> GetTemplate(string reportDate, string reportType)
         {
+            StagingAlgorithm s = new StagingAlgorithm();
+            s.StagingAlgorithm_newdata();
             var connectionString = Configuration.GetConnectionString("destination");
             var dataContext = new DataContext(connectionString);
-
+            dataContext.GenerateExcel();
             var result = dataContext.ExecuteProcedure("CR_SA_xls", reportDate, reportType);
+            return Ok();
+        }
+
+        [HttpPost("PostStagingAlgorithm")]
+        public ActionResult PostStagingAlgorithm()
+        {
+            string[] strings = new string[] { "{-db}", "{hercules}", "{-ip}", "{10.10.0.46}", "{-u}", "{root}", "{-p}", "{admin@2022}" };
+            //var charArray = new MWCharArray(strings);
+            //MWCellArray cellArray = new MWCellArray(charArray);
+            StagingAlgorithm s = new StagingAlgorithm();
+            MWStringArray stringArray = new MWStringArray(strings);
+            s.StagingAlgorithm_newdata(stringArray);
             return Ok();
         }
     }
--- ExcelTemplate/Data/DataContext.cs
+++ ExcelTemplate/Data/DataContext.cs
@@ -1,5 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using System.Data;
+using Syncfusion.XlsIO;
 
 namespace ExcelTemplate.Data
 {
@@ -46,6 +47,38 @@ namespace ExcelTemplate.Data
             }
 
             return result;
+        }
+
+        public void GenerateExcel()
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Excel2016;
+
+                //Open an existing spreadsheet, which will be used as a template for generating the new spreadsheet.
+                //After opening, the workbook object represents the complete in-memory object model of the template spreadsheet.
+                IWorkbook workbook;
+
+                //Open existing Excel template
+                var cfFileStream = new FileStream(@"C:\xls\XlsTemplates\CR_SATemplate.xlsx", FileMode.Open, FileAccess.Read);
+                workbook = excelEngine.Excel.Workbooks.Open(cfFileStream);
+
+                //The first worksheet in the workbook is accessed.
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                ////Create Template Marker processor.
+                ////Apply the marker to export data from datatable to worksheet.
+                //ITemplateMarkersProcessor marker = workbook.CreateTemplateMarkersProcessor();
+                //marker.AddVariable("SalesList", northwindDt);
+                //marker.ApplyMarkers();
+
+                //Saving and closing the workbook
+                FileStream fileStream = new FileStream(@"C:\xls\Output.xlsx", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                workbook.SaveAs(fileStream);
+                //Close the workbook
+                workbook.Close();
+            }
         }
     }
 }
